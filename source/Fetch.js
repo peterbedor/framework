9a725fa447810d8b419b419047b20154bb970c65
Peter Bedor Jr <peter@caddis.co>
2016-09-04 10:57:57 -0500

    Use uppercase verbs and finish request

--- source/Fetch.js
+++ source/Fetch.js
@@ -4,43 +4,106 @@ const _request = new XMLHttpRequest(),
 export default Fetch = {
 	get(options) {
 		return _makeRequest(_extend({
-			method: 'get'
+			method: 'GET'
 		}, options));
 	},
 
 	post(options) {
 		return _makeRequest(_extend({
-			method: 'post'
+			method: 'POST'
 		}, options));
 	},
 
 	put(options) {
 		return _makeRequest(_extend({
-			method: 'put'
+			method: 'PUT'
 		}, options));
 	},
 
 	patch(options) {
 		return _makeRequest(_extend({
-			method: 'patch'
+			method: 'PATCH'
 		}, options));
 	},
 
 	delete(options) {
 		return _makeRequest(_extend({
-			method: 'delete'
+			method: 'DELETE'
 		}, options));
 	}
 };
 
 function _makeRequest(options) {
+	let contentTypeHeader = 'Content-Type',
+		send = null,
+		headers = [];
+
+	if (options.method == 'GET') {
+		options.url = _getUrl(options);
+	} else {
+		var string = typeof options.data == 'string';
+
+		if (! string && ! options.type) {
+			options.type = 'json';
+		}
+
+		send = string || options.processData === false ?
+			options.data :
+			options.type == 'json' ?
+				JSON.stringify(options.data) :
+				Core.$serialize(options.data);
+	}
+
 	_request.open(options.method, options.url);
+
+	// Add content type header
+	if (options.type == 'json') {
+		headers[contentTypeHeader] = 'application/json';
+	} else if (options.type == 'xml') {
+		headers[contentTypeHeader] = 'text/xml';
+	} else if (options.method == 'POST' || options.type == 'form') {
+		headers[contentTypeHeader] =
+			'application/x-www-form-urlencoded';
+	}
+
+	// Accept JSON header
+	if (options.json) {
+		headers.Accept = 'application/json, text/javascript, */*; q=0.01';
+	}
+
+	// Add X-Requested-With header for same domain requests
+	var a = document.createElement('a');
+	a.href = options.url;
+
+	if (! a.host || a.host == location.host) {
+		headers['X-Requested-With'] = 'XMLHttpRequest';
+	}
+
+	// Append character set to content type header
+	headers[contentTypeHeader] += '; charset=UTF-8';
+
+	// Extend configured headers into defaults
+	headers = Core.$extend(headers, options	.headers);
+
+	// Set request headers
+	for (var key in headers) {
+		var val = headers[key];
+
+		if (val !== false) {
+			_request.setRequestHeader(key, val);
+		}
+	}
 
 	if (options.send) {
 		options.send();
 	}
 
-	_request.send();
+	// Set response type
+	if (options.responseType) {
+		_request.responseType = options.responseType;
+	}
+
+	_request.send(send);
 
 	_request.onreadystatechange = function() {
 		_handleStateChange(_request, options)
@@ -78,4 +141,19 @@ function _success(request, options) {
 	}
 
 	options.success(response);
+}
+
+function _getUrl(options) {
+	var url = options.url.replace(/[\?&]$/, '');
+
+	if (options.data && Object.keys(options.data).length) {
+		url += (url.indexOf('?') < 0 ? '?' : '&') +
+			Core.$serialize(options.data);
+	}
+
+	if (url[0] != '/' && ! /^(?:[a-z]+:)?\/\//i.test(url)) {
+		url = '/' + url;
+	}
+
+	return url;
 }
